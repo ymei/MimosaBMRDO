18b8266bed3915b59d712344d66a294294142f6b
wangdong <dongwang.ccnu@qq.com>
2016-08-31 17:54:37 -0700

    initial commit for version_0

--- Software/src/load_FPGA_dong.py
+++ Software/src/load_FPGA_dong.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 __author__ = "Giacomo Contin"
-__version__ = "1.0"
-__email__ = "gcontin@lbl.gov"
+__reviser__= "Dong Wang"
+__version__ = "1.1"
+__email__ = "gcontin@lbl.gov" "dongwang.ccnu@qq.com"
 __status__ = "Prototype"
 
 #!/usr/bin/env python
@@ -13,7 +14,7 @@ from subprocess import call
 
 def load_FPGA():
 	cmdstr1="source /opt/Xilinx/14.7/ISE_DS/settings64.sh" 
-	cmdstr2="impact -batch /home/wangdong/mapstest/impact_dong.cmd"
+	cmdstr2="impact -batch ./impact_dong.cmd"
 	cmdstr_all = cmdstr1+"; "+cmdstr2
 	print str(cmdstr_all)
 	call(cmdstr_all,shell=True)
