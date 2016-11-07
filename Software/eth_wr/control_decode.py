3248f6f9b7de056fc1c3b08c252be2897188557d
WANG DONG <dongwang.ccnu@qq.com>
2016-11-07 10:43:10 -0800

    add command.c for eth_wr

--- Software/eth_wr/control_decode.py
+++ Software/eth_wr/control_decode.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python
-import eth_control_fun as ethc
-import socket               # Import socket module
+#import eth_control_fun as ethc
+#import socket               # Import socket module
 
-s = socket.socket()         # Create a socket object
-host = '192.168.2.3'
-port = 1024                # Reserve a port for your service.
-s.connect((host, port))
+from ctypes import *
+import os
+command = cdll.LoadLibrary(os.getcwd() + '/command.so')
+command.cmd_read_status(str_status, 2)
+print str_status
 
-str_send = ethc.eth_control_reg_write(0,0x0001)
-print str_send
-s.send(str_send)
-str_send = ethc.eth_control_reg_read(0)
-print str_send
-s.send(str_send)
-print s.recv(14)
-
-s.close                     # Close the socket when done
+# s = socket.socket()         # Create a socket object
+# host = '192.168.2.3'
+# port = 1024                # Reserve a port for your service.
+# s.connect((host, port))
+#
+# str_send = ethc.eth_control_reg_write(0,0x0001)
+# print str_send
+# s.send(str_send)
+# str_send = ethc.eth_control_reg_read(0)
+# print str_send
+# s.send(str_send)
+# print s.recv(14)
+#
+# s.close                     # Close the socket when done
